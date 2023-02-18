7683a272e1f80fd3f25c2cc5a92a93adc2c7bc74
algotechpc <115131770+algotechpc@users.noreply.github.com>
2023-02-18 17:30:07 +0530

    function overloading using java

---
+++ Overloadingexample.java
@@ -0,0 +1,44 @@
+
+class Overloadingexample
+{
+ public static void main(String args[])
+   {
+DisplayOverloading obj = new DisplayOverloading();
+	  obj.display('c',5.27);    	
+        obj.display('a',10);
+	  obj.display('a',"JAVA");
+   } //main() end
+}  //class end
+
+class DisplayOverloading
+{
+    public void display(char c,String s)
+    {
+         System.out.println(c + " " +s);
+    }
+    public void display(char c, int num)  
+    {
+         System.out.println(c + " "+num);
+    }
+
+     public void display(char b, double n)  
+    {
+         System.out.println(b + " "+n);
+    }
+}
+
+
+/*
+Overloading is a form of polymorphism,allow the function name to be used with different argument types.
+Memory space is save.
+Multiple behaviors to the same function based on the parameters.
+Your function may want to work with some optional details.
+compile time polymorphism exhibited at compile time. Here, Java compiler knows which method is called.
+static method can be overloaded
+*/
+
+
+
+
+
+
